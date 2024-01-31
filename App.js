5c1c4ac8d9005624efc69ed689efee1b0cbc6675
JuanIFrank <juan.frank@gm2dev.com>
2024-01-31 18:01:26 -0300

    feat(app.js): create main app screnn

--- App.js
+++ App.js
@@ -1,21 +1,102 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { ImageViewer } from "./Components/ImageViewer";
 import { Button } from "./Components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { Circlebutton } from "./Components/CircleButton";
+import { IconButton } from "./Components/IconButton";
+import { EmojiPicker } from "./Components/EmojiPicker";
+import { EmojiList } from "./Components/EmojiList";
+import { EmojiSticker } from "./Components/EmojiSticker";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Animated from "react-native-reanimated";
+
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {};
+
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <ImageViewer
+            placeholderImageSource={PlaceholderImage}
+            selectedImage={selectedImage}
+          />
+          {pickedEmoji && (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          )}
+        </View>
+        <View style={styles.footerContainer}>
+          {showAppOptions ? (
+            <View style={styles.optionsContainer}>
+              <View style={styles.optionsRow}>
+                <IconButton
+                  icon={"refresh"}
+                  label={"Reset"}
+                  onPress={onReset}
+                />
+                <Circlebutton onPress={onAddSticker} />
+                <IconButton
+                  icon={"save-alt"}
+                  label={"Save All"}
+                  onPress={onSaveImageAsync}
+                />
+              </View>
+            </View>
+          ) : (
+            <View>
+              <Button
+                label={"Choose a photo"}
+                theme={"primary"}
+                onPress={pickImageAsync}
+              />
+              <Button
+                label={"Use this photo"}
+                onPress={() => setShowAppOptions(true)}
+              />
+            </View>
+          )}
+        </View>
+        <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+        </EmojiPicker>
+        <StatusBar style="auto" />
       </View>
-      <View style={styles.footerContainer}>
-        <Button label={"Choose a phoot"} theme={"primary"} />
-        <Button label={"Use this photo"} />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -33,5 +114,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
